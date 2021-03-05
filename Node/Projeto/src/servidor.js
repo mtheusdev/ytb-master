35ec428191d59722f949ed451fe418a708646678
Matheus Henrique <08990349931@edu.udesc.br>
2021-03-05 17:01:29 -0300

    Node

---
+++ Node/Projeto/src/servidor.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Hello')
+})
+
+app.listen('3030', () => {
+    console.log('Servidor Rodando')
+})
